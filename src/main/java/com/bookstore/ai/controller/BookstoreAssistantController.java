e947ed6f51faa58057a74f7120ea8fdeebdbba7d
Helio Dourado <heliohdd@yahoo.com.br>
2024-04-17 08:38:42 -0300

    Método com PromptTemplate para reviews de livros

--- src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
@@ -1,5 +1,6 @@
 package com.bookstore.ai.controller;
 
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,27 @@ public class BookstoreAssistantController {
 			@RequestParam(value = "message", defaultValue = "Quais são considerados os best sellers nestes últimos anos?") 
 			String message) {
 		return chatClient.call(message);
+	}
+
+//	@GetMapping(value = "/informations")
+//	public ChatResponse bookstoreChatEx2(
+//			@RequestParam(value = "message", defaultValue = "Quais são considerados os best sellers nestes últimos anos?") 
+//			String message) {
+//		return chatClient.call(new Prompt(message));
+//	}
+	
+	@GetMapping(value = "/reviews")
+	public String bookstoreReview(@RequestParam(
+			value = "book", 
+			defaultValue = "Dom Quixote")
+			String book) {
+		PromptTemplate promptTemplate = new PromptTemplate("""
+				Por favor, me forneça
+				um breve resumo do livro {book}
+				e também a biografia de seu autor.
+				""");
+		promptTemplate.add("book", book);
+		return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
 	}
 
 }
